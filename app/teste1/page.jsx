1c06c74b82844052826b656b4427f1646ceac523
Jonatas-Roque <jonatasroque@ua.pt>
2023-09-28 09:32:24 +0100

    teste1 page update

--- app/teste1/page.jsx
+++ app/teste1/page.jsx
@@ -1,10 +1,14 @@
 /* "USE CLIENT"*/
 import React from "react"
+import Image from "next/image"
 
 const Teste1 = () => {
 
     return(
-        <div>Olá</div>
+        <div className="my-7 mx-auto text-center font-semibold text-[20px]">
+            <div>Olá</div>
+            <Image className='mx-auto w-[60%] h-[50%] mt-10' src='/imgs/shrek.jpg' alt='Shrek' sizes='100vw' width={0} height={0}/>
+            </div>
     )
 
 }
