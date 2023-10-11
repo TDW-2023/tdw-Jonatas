9cf45c35c06da9eb491d9a9c320a318e9bfd9d54
Jonatas-Roque <jonatasroque@ua.pt>
2023-10-11 16:21:54 +0100

    jest integration

---
+++ __tests__/index.test.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Teste2 from '@/app/teste2/page';
+import '@testing-library/jest-dom';
+
+
+test('renders message correctly', () => {
+  const messageText = 'Hello, World!';
+  render(<Teste2 text={messageText} />);
+  
+  // Assert that the message text is rendered
+  const messageElement = screen.getByText(messageText);
+  expect(messageElement).toBeInTheDocument();
+
+  // You can add more assertions as needed
+});
---
+++ components/sumtest.jsx
@@ -0,0 +1,11 @@
+
+
+const Sum = () => {
+
+       var teste = 1;
+       
+       return(teste)
+
+}
+
+export default Sum;
