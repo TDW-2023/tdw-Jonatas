879234212657586fa4f70caa60f3977b606896ea
Jonatas-Roque <jonatasroque@ua.pt>
2023-10-12 11:32:07 +0100

    .

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,14 +1,12 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Teste2 from "@/app/teste2/page";
+import "@testing-library/jest-dom";
 
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Teste2 from '@/app/teste2/page';
-import '@testing-library/jest-dom';
-
-
-test('renders message correctly', () => {
-  const messageText = 'Hello, World!';
+test("renders message correctly", () => {
+  const messageText = "Hello, World!";
   render(<Teste2 text={messageText} />);
-  
+
   // Assert that the message text is rendered
   const messageElement = screen.getByText(messageText);
   expect(messageElement).toBeInTheDocument();
